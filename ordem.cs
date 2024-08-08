dc3a2fdeb81d4fcab266fcc01c4704d8f7022d6c
CiscG <fgss98@gmail.com>
2024-08-08 23:35:09 +0000

    mudanca codigo 2

--- ordem.cs
+++ ordem.cs
@@ -3,15 +3,12 @@ using System:
 
 public class Ordem{
     int[] ordenado[];
-
     for(i = 0; i < 5; i++)
     {
         ordenado[i] = Console.ReadLine();
-        
     }
     int aux = null;
     for(int i = 0; i < 5; i++)
-    
         for(int a = i+1; a < 5; a++)
         {
             if(ordenado[i] < ordenado[a]){
