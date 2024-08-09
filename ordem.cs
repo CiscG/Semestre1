3164f5da7d7eb1f7906cf65a3620e00d606f82bf
CiscG <80540828+CiscG@users.noreply.github.com>
2024-08-09 10:06:32 -0300

    Update ordem.cs

--- ordem.cs
+++ ordem.cs
@@ -3,26 +3,21 @@ using System:
 public class Ordem{
     int[] ordenado[];
     for(i = 0; i < 5; i++){
-    {
         ordenado[i] = Console.ReadLine();
     }
-        int aux = null;
-        int count;
-        for(int i = 0; i < 4; i++){
-            for(int a = i+1; a < 5; a++){
-                if(ordenado[i] < ordenado[a]){
-                    aux = ordenadi[a];
-                    count = a;
-                }
-            }         
-
-            if(aux != null){
-                ordenado[count] = ordenado[i];
-                ordenado[i] = aux;
-                aux = null;
-
-                
+    int aux = null;
+    int count;
+    for(int i = 0; i < 4; i++){
+        for(int a = i+1; a < 5; a++){
+            if(ordenado[i] < ordenado[a]){
+                aux = ordenadi[a];
+                count = a;
             }
+        }         
+        if(aux != null){
+            ordenado[count] = ordenado[i];
+            ordenado[i] = aux;
+            aux = null;       
         }
     }
 }
