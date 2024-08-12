2b82721d88f74ebe0981182ef4a7554b044ff56b
CiscG <80540828+CiscG@users.noreply.github.com>
2024-08-12 02:42:49 -0300

    teste java

---
+++ Caneta.java
@@ -0,0 +1,22 @@
+package Semestre1;
+public class Caneta{
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    void status(){
+        System.out.println("Uma caneta " + this.cor/* +" de ponta " + this.ponta + " de carga " + this.carga*/);
+    }
+    
+   /* void rabiscar(){
+        
+    }
+    void tampar(){
+        
+    }
+    void destampar(){
+        
+    }*/
+}
