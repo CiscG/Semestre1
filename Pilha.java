6e131d85dc46dccb8fad79d3196abbc8be097b71
CiscG <fgss98@gmail.com>
2024-08-22 20:11:59 -0300

    add Pilha.java

---
+++ Pilha.java
@@ -0,0 +1,18 @@
+package semestre1;
+public class Pilha {
+    private int tamMax;
+    private Object item[];
+    private int topo;
+
+
+    public void Pilha(){
+        tamMax = 5;
+        item = new Object[5];
+        topo = -1;
+    }
+
+    /*public Object empilhar(Object insert){
+        
+    }*/
+}
+
