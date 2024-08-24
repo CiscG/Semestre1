6843b7cfd25b0285374c1e447ec1c9fd8888cd2e
CiscG <fgss98@gmail.com>
2024-08-24 01:05:32 +0000

    att

--- Aula_04.java
+++ Aula_04.java
@@ -3,7 +3,7 @@ package semestre1;
 public class Aula_04{
 	public static void main(String[] args){
 		Pessoa marcos = new Pessoa("Marcos",23,"Masc");
-
+		System.out.println(marcos.falar());
 	}
 }
 
