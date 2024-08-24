eb50ea1713e248321b6799ee2221175e166aa154
CiscG <fgss98@gmail.com>
2024-08-24 01:00:00 +0000

    att

--- Aula_04.java
+++ Aula_04.java
@@ -2,7 +2,7 @@ package semestre1;
 
 public class Aula_04{
 	public static void main(String[] args){
-		Pessoa marcos = new Pessoa();
+		Pessoa marcos = new Pessoa("Marcos",23,"Masc");
 
 	}
 }
--- Pessoa.java
+++ Pessoa.java
@@ -3,13 +3,14 @@ public Class Pessoa{
 	int idade;
 	String nome;
 	String sexo;
-	public String fala(){
-		return nome;
-	}
 
 	public Pessoa(String nome, int idade, String sexo){
 		this.nome = nome;
 		this.idade = idade;
 		this.sexo = sexo;
+	}
+
+	public String falar(){
+		return this.nome;
 	}
 }
